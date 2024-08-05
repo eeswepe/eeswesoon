45920601e472e1ef855b9d7020af91e25bc893f6
singgih wahyu permana <singgihwahyupermana@gmail.com>
2024-08-05 14:23:00 +0000

    initiating desktop mode

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,13 @@ import Header from './components/Header.jsx';
 import Jumbotron from './components/Jumbotron.jsx';
 import Project from './components/Project.jsx';
 import Profile from './components/Profile.jsx';
+import Desktop from './components/Desktop.jsx';
 
 export default function App() {
   return (
     <main>
       <div className="App">
+        <Desktop/>
         <Header />
         <Jumbotron />
         <Project />
---
+++ src/components/Desktop.jsx
@@ -0,0 +1,11 @@
+import "./Desktop.css";
+
+const Desktop = () => {
+  return (
+    <div className="desktop-view">
+      <h1>Desktop Mode is Under Construction</h1>
+    </div>
+  );
+};
+
+export default Desktop;
